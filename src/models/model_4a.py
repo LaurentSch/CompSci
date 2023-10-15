238474d792cb3428bb35d02bea35a66dcdd91821
LaurentSch <48076819+LaurentSch@users.noreply.github.com>
2023-10-15 18:17:38 +0200

    Fix hessian 5th loop

--- src/models/model_4a.py
+++ src/models/model_4a.py
@@ -147,15 +147,11 @@ def derivatives(u, g):
         u3 = u[(2 * j + 2 + 2 * floor((j - 1) / 9)) - 1]
         u4 = u[(2 * j + 2 * floor((j - 1) / 9)) - 1]
         # derivative for u[2*i-1]
-        f1 = (2 * (-u2 + u1 + 1) * (sqrt((u3 - u4) ** 2 + (-u2 + u1 + 1) ** 2) - 1)) / sqrt(
-            (u3 - u4) ** 2 + (-u2 + u1 + 1) ** 2)
+        f1 = (2*(-u2+u1+1)*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1))/sqrt((u3-u4)**2+(-u2+u1+1)**2)
         # derivative for u[2*i]
-        f2 = -((2 * (-u2 + u1 + 1) * (sqrt((u3 - u4) ** 2 + (-u2 + u1 + 1) ** 2) - 1)) / sqrt(
-            (u3 - u4) ** 2 + (-u2 + u1 + 1) ** 2))
-        f3 = (2 * (sqrt((u3 - u4) ** 2 + (-u2 + u1 + 1) ** 2) - 1) * (u3 - u4)) / sqrt(
-            (u3 - u4) ** 2 + (-u2 + u1 + 1) ** 2)
-        f4 = -((2 * (sqrt((u3 - u4) ** 2 + (-u2 + u1 + 1) ** 2) - 1) * (u3 - u4)) / sqrt(
-            (u3 - u4) ** 2 + (-u2 + u1 + 1) ** 2))
+        f2 = -((2*(-u2+u1+1)*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1))/sqrt((u3-u4)**2+(-u2+u1+1)**2))
+        f3 = (2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1)*(u3-u4))/sqrt((u3-u4)**2+(-u2+u1+1)**2)
+        f4 = -((2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1)*(u3-u4))/sqrt((u3-u4)**2+(-u2+u1+1)**2))
         # Add results to the gradient
         f_new[(2 * j + 1 + 2 * floor((j - 1) / 9)) - 1] += f1
         f_new[(2 * j - 1 + 2 * floor((j - 1) / 9)) - 1] += f2
@@ -224,7 +220,7 @@ def sec_derivatives(u, g):
         u2i = (2 * j) - 1
         u1 = u[u1i]
         u2 = u[u2i]
-        # "p1: Sec-Derivative u1, u1:"^
+        # "p1: Sec-Derivative u1, u1:"
         f1 = (2*(sqrt((u2+1)**2+u1**2)-1))/sqrt((u2+1)**2+u1**2)-(2*u1**2*(sqrt((u2+1)**2+u1**2)-1))/((u2+1)**2+u1**2)**(3/2)+(2*u1**2)/((u2+1)**2+u1**2)
         update_matrix_value(matrix, u1i, u1i, f1)
 
@@ -240,10 +236,6 @@ def sec_derivatives(u, g):
         # p1: Sec-Derivative u2, u2:
         f4 = (2*(sqrt((u2+1)**2+u1**2)-1))/sqrt((u2+1)**2+u1**2)-(2*(u2+1)**2*(sqrt((u2+1)**2+u1**2)-1))/((u2+1)**2+u1**2)**(3/2)+(2*(u2+1)**2)/((u2+1)**2+u1**2)
         update_matrix_value(matrix, u2i, u2i, f4)
-
-    plt.imshow(matrix, cmap='viridis')  # You can choose different colormaps
-    plt.colorbar()
-    plt.show()
 
     for i in range(9):
         # fix so that it goes from 1 to 10 instead of 0 to 9
@@ -265,10 +257,6 @@ def sec_derivatives(u, g):
         # p2: Sec-Derivative u2, u2:
         f4 = (2*(sqrt((u2+1)**2+(u1+1)**2)-sqrt(2)))/sqrt((u2+1)**2+(u1+1)**2)-(2*(u2+1)**2*(sqrt((u2+1)**2+(u1+1)**2)-sqrt(2)))/((u2+1)**2+(u1+1)**2)**(3/2)+(2*(u2+1)**2)/((u2+1)**2+(u1+1)**2)
         update_matrix_value(matrix, u2i, u2i, f4)
-
-    plt.imshow(matrix, cmap='viridis')  # You can choose different colormaps
-    plt.colorbar()
-    plt.show()
 
     for i in range(9):
         # part 3 loop
@@ -295,10 +283,6 @@ def sec_derivatives(u, g):
         f4 = (2*(sqrt((u2+1)**2+(1-u1)**2)-sqrt(2)))/sqrt((u2+1)**2+(1-u1)**2)-(2*(u2+1)**2*(sqrt((u2+1)**2+(1-u1)**2)-sqrt(2)))/((u2+1)**2+(1-u1)**2)**(3/2)+(2*(u2+1)**2)/((u2+1)**2+(1-u1)**2)
         update_matrix_value(matrix, u2i, u2i, f4)
         # matrix[u2i][u2i] += f4
-
-    plt.imshow(matrix, cmap='viridis')  # You can choose different colormaps
-    plt.colorbar()
-    plt.show()
 
     for i in range(30):
         # part 4 loop
@@ -359,10 +343,6 @@ def sec_derivatives(u, g):
         f16 = (2*(u3-u4)**2)/((u3-u4)**2+(-u2+u1+1)**2)-(2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1)*(u3-u4)**2)/((u3-u4)**2+(-u2+u1+1)**2)**(3/2)+(2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1))/sqrt((u3-u4)**2+(-u2+u1+1)**2)
         update_matrix_value(matrix, u4i, u4i, f16)
 
-    plt.imshow(matrix, cmap='viridis')  # You can choose different colormaps
-    plt.colorbar()
-    plt.show()
-
     for i in range(36):
         # part 5 loop
         # fix so that it goes from 1 to 10 instead of 0 to 9
@@ -377,54 +357,50 @@ def sec_derivatives(u, g):
         u4 = u[u4i]
 
         # p6: Sec-Derivative u1, u1:
-        f1 = (2*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2)))/sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)+(2*(-u2+u1+1)**2)/((-u4+u3+1)**2+(-u2+u1+1)**2)-(2*(-u2+u1+1)**2*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2)))/((-u4+u3+1)**2+(-u2+u1+1)**2)**(3/2)
+        f1 = (2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1))/sqrt((u3-u4)**2+(-u2+u1+1)**2)+(2*(-u2+u1+1)**2)/((u3-u4)**2+(-u2+u1+1)**2)-(2*(-u2+u1+1)**2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1))/((u3-u4)**2+(-u2+u1+1)**2)**(3/2)
         update_matrix_value(matrix, u1i, u1i, f1)
 
         # p6: Sec-Derivative u1, u2:
-        f2 = -((2*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2)))/sqrt((-u4+u3+1)**2+(-u2+u1+1)**2))-(2*(-u2+u1+1)**2)/((-u4+u3+1)**2+(-u2+u1+1)**2)+(2*(-u2+u1+1)**2*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2)))/((-u4+u3+1)**2+(-u2+u1+1)**2)**(3/2)
+        f2 = -((2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1))/sqrt((u3-u4)**2+(-u2+u1+1)**2))-(2*(-u2+u1+1)**2)/((u3-u4)**2+(-u2+u1+1)**2)+(2*(-u2+u1+1)**2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1))/((u3-u4)**2+(-u2+u1+1)**2)**(3/2)
         update_matrix_value(matrix, u1i, u2i, f2)
         update_matrix_value(matrix, u2i, u1i, f2)
 
         # p6: Sec-Derivative u1, u3:
-        f3 = (2*(-u2+u1+1)*(-u4+u3+1))/((-u4+u3+1)**2+(-u2+u1+1)**2)-(2*(-u2+u1+1)*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2))*(-u4+u3+1))/((-u4+u3+1)**2+(-u2+u1+1)**2)**(3/2)
+        f3 = (2*(-u2+u1+1)*(u3-u4))/((u3-u4)**2+(-u2+u1+1)**2)-(2*(-u2+u1+1)*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1)*(u3-u4))/((u3-u4)**2+(-u2+u1+1)**2)**(3/2)
         update_matrix_value(matrix, u1i, u3i, f3)
         update_matrix_value(matrix, u3i, u1i, f3)
 
         # p6: Sec-Derivative u1, u4:
-        f4 = (2*(-u2+u1+1)*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2))*(-u4+u3+1))/((-u4+u3+1)**2+(-u2+u1+1)**2)**(3/2)-(2*(-u2+u1+1)*(-u4+u3+1))/((-u4+u3+1)**2+(-u2+u1+1)**2)
+        f4 = (2*(-u2+u1+1)*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1)*(u3-u4))/((u3-u4)**2+(-u2+u1+1)**2)**(3/2)-(2*(-u2+u1+1)*(u3-u4))/((u3-u4)**2+(-u2+u1+1)**2)
         update_matrix_value(matrix, u1i, u4i, f4)
         update_matrix_value(matrix, u4i, u1i, f4)
 
         # p6: Sec-Derivative u2, u2:
-        f6 = (2*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2)))/sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)+(2*(-u2+u1+1)**2)/((-u4+u3+1)**2+(-u2+u1+1)**2)-(2*(-u2+u1+1)**2*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2)))/((-u4+u3+1)**2+(-u2+u1+1)**2)**(3/2)
+        f6 = (2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1))/sqrt((u3-u4)**2+(-u2+u1+1)**2)+(2*(-u2+u1+1)**2)/((u3-u4)**2+(-u2+u1+1)**2)-(2*(-u2+u1+1)**2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1))/((u3-u4)**2+(-u2+u1+1)**2)**(3/2)
         update_matrix_value(matrix, u2i, u2i, f6)
 
         # p6: Sec-Derivative u2, u3:
-        f7 = (2*(-u2+u1+1)*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2))*(-u4+u3+1))/((-u4+u3+1)**2+(-u2+u1+1)**2)**(3/2)-(2*(-u2+u1+1)*(-u4+u3+1))/((-u4+u3+1)**2+(-u2+u1+1)**2)
+        f7 = (2*(-u2+u1+1)*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1)*(u3-u4))/((u3-u4)**2+(-u2+u1+1)**2)**(3/2)-(2*(-u2+u1+1)*(u3-u4))/((u3-u4)**2+(-u2+u1+1)**2)
         update_matrix_value(matrix, u2i, u3i, f7)
         update_matrix_value(matrix, u3i, u2i, f7)
 
         # p6: Sec-Derivative u2, u4:
-        f8 = (2*(-u2+u1+1)*(-u4+u3+1))/((-u4+u3+1)**2+(-u2+u1+1)**2)-(2*(-u2+u1+1)*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2))*(-u4+u3+1))/((-u4+u3+1)**2+(-u2+u1+1)**2)**(3/2)
+        f8 = (2*(-u2+u1+1)*(u3-u4))/((u3-u4)**2+(-u2+u1+1)**2)-(2*(-u2+u1+1)*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1)*(u3-u4))/((u3-u4)**2+(-u2+u1+1)**2)**(3/2)
         update_matrix_value(matrix, u2i, u4i, f8)
         update_matrix_value(matrix, u4i, u2i, f8)
 
         # p6: Sec-Derivative u3, u3:
-        f11 = (2*(-u4+u3+1)**2)/((-u4+u3+1)**2+(-u2+u1+1)**2)-(2*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2))*(-u4+u3+1)**2)/((-u4+u3+1)**2+(-u2+u1+1)**2)**(3/2)+(2*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2)))/sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)
+        f11 = (2*(u3-u4)**2)/((u3-u4)**2+(-u2+u1+1)**2)-(2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1)*(u3-u4)**2)/((u3-u4)**2+(-u2+u1+1)**2)**(3/2)+(2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1))/sqrt((u3-u4)**2+(-u2+u1+1)**2)
         update_matrix_value(matrix, u3i, u3i, f11)
 
         # p6: Sec-Derivative u3, u4:
-        f12 = -((2*(-u4+u3+1)**2)/((-u4+u3+1)**2+(-u2+u1+1)**2))+(2*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2))*(-u4+u3+1)**2)/((-u4+u3+1)**2+(-u2+u1+1)**2)**(3/2)-(2*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2)))/sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)
+        f12 = -((2*(u3-u4)**2)/((u3-u4)**2+(-u2+u1+1)**2))+(2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1)*(u3-u4)**2)/((u3-u4)**2+(-u2+u1+1)**2)**(3/2)-(2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1))/sqrt((u3-u4)**2+(-u2+u1+1)**2)
         update_matrix_value(matrix, u3i, u4i, f12)
         update_matrix_value(matrix, u4i, u3i, f12)
 
         # p6: Sec-Derivative u4, u4:
-        f16 = (2*(-u4+u3+1)**2)/((-u4+u3+1)**2+(-u2+u1+1)**2)-(2*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2))*(-u4+u3+1)**2)/((-u4+u3+1)**2+(-u2+u1+1)**2)**(3/2)+(2*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2)))/sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)
+        f16 = (2*(u3-u4)**2)/((u3-u4)**2+(-u2+u1+1)**2)-(2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1)*(u3-u4)**2)/((u3-u4)**2+(-u2+u1+1)**2)**(3/2)+(2*(sqrt((u3-u4)**2+(-u2+u1+1)**2)-1))/sqrt((u3-u4)**2+(-u2+u1+1)**2)
         update_matrix_value(matrix, u4i, u4i, f16)
-    print("After 5th loop")
-    plt.imshow(matrix, cmap='viridis')  # You can choose different colormaps
-    plt.colorbar()
-    plt.show()
 
     for i in range(27):
         # part 6 loop
@@ -485,9 +461,6 @@ def sec_derivatives(u, g):
         f16 = (2*(-u4+u3+1)**2)/((-u4+u3+1)**2+(-u2+u1+1)**2)-(2*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2))*(-u4+u3+1)**2)/((-u4+u3+1)**2+(-u2+u1+1)**2)**(3/2)+(2*(sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)-sqrt(2)))/sqrt((-u4+u3+1)**2+(-u2+u1+1)**2)
         update_matrix_value(matrix, u4i, u4i, f16)
 
-    plt.imshow(matrix, cmap='viridis')  # You can choose different colormaps
-    plt.colorbar()
-    plt.show()
 
     for i in range(27):
         # part 7 loop
@@ -547,11 +520,6 @@ def sec_derivatives(u, g):
         # p7: Sec-Derivative u4, u4:
         f16 = (2*(-u4+u3+1)**2)/((-u4+u3+1)**2+(u2-u1+1)**2)-(2*(sqrt((-u4+u3+1)**2+(u2-u1+1)**2)-sqrt(2))*(-u4+u3+1)**2)/((-u4+u3+1)**2+(u2-u1+1)**2)**(3/2)+(2*(sqrt((-u4+u3+1)**2+(u2-u1+1)**2)-sqrt(2)))/sqrt((-u4+u3+1)**2+(u2-u1+1)**2)
         update_matrix_value(matrix, u4i, u4i, f16)
-
-    plt.imshow(matrix, cmap='viridis')  # You can choose different colormaps
-    plt.colorbar()
-    plt.show()
-
     return matrix
 
 
